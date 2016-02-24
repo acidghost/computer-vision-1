7251ab8a97d5aa9be123497bdb36f1de7e7f3fa2
selBaez <sel.bs.93@hotmail.com>
2016-02-24 15:32:30 +0100

    worked on 1.3

--- 2-assignment/gaussianDer.m
+++ 2-assignment/gaussianDer.m
@@ -1,18 +1,33 @@
-function [ imOut Gd] = gaussianDer (im_path ,G, sigma )
+function [ imOut Gd] = gaussianDer (im_path , G, sigma )
+
+kernel_length = size(G);
+siz = (kernel_length-1) / 2;
+x = -siz:siz;
+
+Gd = - (x.* G) / (sigma^2);
 
 % Load image
 im = im2double(imread(im_path));
 [size_y, size_x, size_c] = size(im); 
+size(im)
 
-% Apply Gaussian kernel to image
-% im_out2 = zeros(size_y, size_x, size_c);
-for k=1:size_c;
-    for j=0:size_x;
-        for i=0:size_y;
-            Gd = - (im(i, j, k) * G) / (sigma^2);
-            imOut(i, j, k) = Gd;
-        end
-    end
+
+% Apply 2D kernel to image
+for i=1:size_c;
+    imOut(:,:,i) = conv2(im(:,:,i), Gd, 'full');
 end
+size(imOut)
+
+% Show original image
+figure
+subplot(1,2,1);
+imshow(im, []);
+title('Original');
+s_str = num2str(sigma);
+% Show image filtered with 2D kernel
+%subplot(1,2,2);
+figure
+imshow(imOut, []);
+title(['2D filtered _{\sigma=', s_str, '}']);
 
 end
