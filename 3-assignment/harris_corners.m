9dff308c2e81ad4b94ccd4ce5d17706f547a2734
Andrea Jemmett <andreajemmett@gmail.com>
2016-03-02 15:51:30 +0100

    added some comments

--- 3-assignment/harris_corners.m
+++ 3-assignment/harris_corners.m
@@ -6,7 +6,7 @@ im_gray = rgb2gray(im_rgb);
 im = im2double(im_gray);
 
 
-% Define gaussians
+%% Compute gaussian partial derivatives
 siz = (kernel_length-1) / 2;
 kernel_size = -siz:siz;
 [Xk, Yk] = meshgrid(kernel_size);
@@ -16,24 +16,30 @@ kernel_y = kernel_x';
 kernel_yd = (Yk .* kernel_y) / (sigma^2);
 
 
-% Convolve image with guassian derivative
+%% Convolve image with guassian derivative
+% to obtain gradients in X and Y directions
 Ix = conv2(im, kernel_xd, 'same');
-figure, imagesc(Ix), colormap gray, title('Image gradient in X direction I_x');
 Iy = conv2(im, kernel_yd, 'same');
-figure, imagesc(Iy), colormap gray, title('Image gradient in Y direction I_y');
+figure % Plot gradient images
+subplot 121, imagesc(Ix), colormap gray, title('Image gradient in X direction I_x');
+subplot 122, imagesc(Iy), colormap gray, title('Image gradient in Y direction I_y');
 
 
 % Define large gaussian 
-g = fspecial('gaussian',max(1,fix(6*sigma)), sigma);
-
+g = fspecial('gaussian',max(1,fix(6 * window_size)), sigma);
+% Convolve with gaussian
 Ix2 = conv2(Ix.^2, g, 'same');
 Iy2 = conv2(Iy.^2, g, 'same');
-Ixy = conv2(Ix.*Iy, g,'same');
+Ixy = conv2(Ix.*Iy, g, 'same');
 
 
+%% Compute the 'cornerness' matrix H
 A = Ix2; B = Ixy; C = Iy2;
 H = (A.*C - B.^2) - 0.04 * ((A + C).^2);
+% Scale H
+H = (1000 / max(max(H))) * H;
 
+%% Find local maxima within the window
 nrows = size(H, 1);
 ncols = size(H, 2);
 k = 1;
@@ -44,7 +50,7 @@ for y=1+half_winsize:nrows-half_winsize;
     for x=1+half_winsize:ncols-half_winsize;
         window = H((y-half_winsize):(y+half_winsize), (x-half_winsize):(x+half_winsize));
         p = H(y, x);
-        if p > threshold && sum(sum(p > window)) == length(window) - 1
+        if p > threshold && sum(sum(p > window)) == (window_size^2) - 1
             r(k, 1) = y;
             c(k, 1) = x;
             k = k + 1;
@@ -54,9 +60,10 @@ end
 r = r(r ~= 0);
 c = c(c ~= 0);
 
-figure, imshow(im_rgb), hold on
-plot(r, c, 'b+', 'MarkerSize', 8)
-title(sprintf('Detected features with n=%d \\sigma=%f', window_size, sigma))
+%% Plot feature points found over original image
+figure, imagesc(im_rgb)
+hold on, plot(r, c, 'bo')
+title(sprintf('Detected features with window\\_size=%d \\sigma=%.2f', window_size, sigma))
 hold off
 
 end
--- 3-assignment/harris_test.m
+++ 3-assignment/harris_test.m
@@ -2,7 +2,7 @@ close all
 
 im_path = 'assets/pingpong/0000.jpeg';
 kernel_length = 11;
-sigma = 1;
-window_size = 35;
-threshold = 0;
+sigma = 1.2;
+window_size = 15;
+threshold = 8;
 [H, r, c] = harris_corners(im_path, kernel_length, sigma, window_size, threshold);
