680077b23c9286c30c17c55c4c9708219af0c9fa
Andrea Jemmett <andreajemmett@gmail.com>
2016-03-05 17:24:31 +0100

    fixed tracking (use round instead of floor!)

--- 3-assignment/rm_keypoints.m
+++ 3-assignment/rm_keypoints.m
@@ -1,15 +1,15 @@
-function [ keypoints_r, keypoints_c ] = rm_keypoints( keypoints, half_region, imsize )
-%RM_KEYPOINTS
-% Remove keypoints for which the corrisponding region
-% falls off the image boundaries
+function [ keypoints_r, keypoints_c ] = rm_keypoints( keypoints, region_size, imsize )
+%RM_KEYPOINTS Remove keypoints for which the corrisponding region
+%             falls off the image boundaries
+half_region = floor(region_size / 2);
 j = 1;
 keypoints_r = zeros(size(keypoints, 1), 1);
 keypoints_c = zeros(size(keypoints, 1), 1);
 for i = 1:size(keypoints, 1)
     kpy = keypoints(i, 1);
     kpx = keypoints(i, 2);
-    if kpy-half_region > 0 && kpy+half_region < imsize(1)-1 && ...
-            kpx-half_region > 0 && kpx+half_region < imsize(2)-1
+    if kpy-half_region > 0 && kpy+half_region < imsize(1)+1 && ...
+            kpx-half_region > 0 && kpx+half_region < imsize(2)+1
         keypoints_r(j) = kpy;
         keypoints_c(j) = kpx;
         j = j + 1;
--- 3-assignment/tracking_test.m
+++ 3-assignment/tracking_test.m
@@ -1,29 +1,17 @@
-
 clear, close all
 
 
-% im_format = 'assets/person_toy/000000%02d.jpg';
-% nfirst = 1;
-% nim = 104;
-im_format = 'assets/pingpong/00%02d.jpeg';
-nfirst = 0;
-nim = 52;
+im_format = 'assets/person_toy/00000%03d.jpg';
+nfirst = 1;
+nim = 104;
+% im_format = 'assets/pingpong/00%02d.jpeg';
+% nfirst = 0;
+% nim = 52;
+
 im_indexes = nfirst+1:nim;
 
 im_first = imread(sprintf(im_format, nfirst));
 [nrows, ncols, nchan] = size(im_first);
-
-ims_rgb = zeros(nrows, ncols, nchan, length(im_indexes) + 1);
-ims = zeros(nrows, ncols, length(im_indexes) + 1);
-ims_rgb(:, :, :, 1) = im_first(:, :, :);
-ims(:, :, 1) = im2double(rgb2gray(im_first));
-
-for i = im_indexes
-    impath = sprintf(im_format, i);
-    im = imread(impath);
-    ims_rgb(:, :, :, i+1) = im(:, :, :);
-    ims(:, :, i+1) = im2double(rgb2gray(im));
-end
 
 
 kernel_length = 11;
@@ -33,25 +21,27 @@ threshold = 7;
 [H, r, c] = harris_corners(sprintf(im_format, nfirst), kernel_length, sigma, window_size, threshold);
 
 
-regions_size = 5;
-half_region = floor(regions_size / 2);
+regions_size = 15;
 
 
 u = r; v = c;
 figure
 for i = im_indexes
-    j = i - 1;
-    impath1 = sprintf(im_format, j);
-    impath2 = sprintf(im_format, j + 1);
+    impath1 = sprintf(im_format, i - 1);
+    impath2 = sprintf(im_format, i);
+    imrgb = imread(impath1);
 
-    [ u, v ] = rm_keypoints(floor([u v]), half_region, size(im_first));
+    ksize = size(u, 1);
+    [ u, v ] = rm_keypoints(round([u v]), regions_size, size(imrgb));
+    fprintf('Removed %d keypoints (outside viewport)\n', ksize - size(u, 1));
 
-    [ du, dv ] = lucas_kanade( impath1, impath2, regions_size, 0, 0, [u v] );
+    % use dv, du instead of du, dv
+    [ dv, du ] = lucas_kanade( impath1, impath2, regions_size, 0, 0, [u v] );
     
-    imagesc(ims(:, :, i)), colormap gray, hold on
-    plot(u, v, 'bo')
-    quiver(u, v, du, dv, 1, 'r'), hold off
-    drawnow, pause(.5)
+    imagesc(imrgb), hold on
+    plot(v, u, 'bo')
+    quiver(v, u, dv, du, 1, 'r'), hold off
+    drawnow, pause(.1)
     
     u = u + du;
     v = v + dv;
