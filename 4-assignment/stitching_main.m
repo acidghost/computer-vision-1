39850bf5f5569ecb1e38f7cd80baf4c6d94048d9
selBaez <sel.bs.93@hotmail.com>
2016-03-12 13:01:05 +0100

    checkpoint

--- 4-assignment/stitching_main.m
+++ 4-assignment/stitching_main.m
@@ -6,7 +6,8 @@ impath2 = 'assets/right.jpg';
 im1 = imread(impath1);
 im2 = imread(impath2);
 
-[imsizey, imsizex, channels] = size(im2);
+[im1sizey, im1sizex, channels] = size(im1);
+[im2sizey, im2sizex, ~] = size(im2);
 
 if channels ~= 1
     im1 = rgb2gray(im1);
@@ -19,30 +20,39 @@ im2 = im2single(im2);
 [frames1, frames2, matches] = get_matches(im1, im2);
 
 %% Set up and perform RANSAC
-N = 1;
+N = 5;
 P = 3;
 radius = 10;
 [best_params, inliers_count] = ransac(N, P, radius, frames1, frames2, matches, im1, im2);
 
 
 %% Estimate length
-[estimated_x estimated_y] = find_transformation_bound([imsizey imsizex], best_params);
+[xsize, ysize, xmin, ymin] = find_transformation_bound([im2sizey, im2sizex], best_params);
+
+estimated_x = round(im1sizex + xsize - xmin);
+%max([im1sizex im2sizex xsize]);
+estimated_y = round(im1sizey + ysize - ymin);
+%max([im1sizey im2sizey ysize]);
 
  
 %% Stitch
 new_image = zeros(estimated_y, estimated_x);
 
-im2_t = transform_image(im1, best_params);
+im2_t = transform_image(im2, best_params, 1);
+figure
+imshow(im2_t);
 
 for i = 1:estimated_y
    for j = 1:estimated_x
        % copy first image
-       if(i <= imsizey) && (j  <= imsizex)
+       if(i <= im1sizey) && (j  <= im1sizex)
            new_image(i, j) = im1(i, j);
-       elseif (i <= imsizey)
-           new_image(i, j) = im2_t(i, j-imsizex);
-       else 
-           new_image(i, j) = im2_t(i-imsizey, j);
+       elseif (i <= im1sizey)
+           new_image(i, j) = im2_t(i, j-im1sizex);
+       elseif (j  <= im1sizex)
+           new_image(i, j) = im2_t(i-im1sizey, j);
+       else
+           new_image(i, j) = im2_t(i-im1sizey, j-im1sizex);
        end
    end
 end
