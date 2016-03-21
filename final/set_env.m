e79e322888134513c13f96e3c558ff866f7b2806
Andrea Jemmett <andreajemmett@gmail.com>
2016-03-21 10:53:29 +0100

    set libraries env

---
+++ final/set_env.m
@@ -0,0 +1,12 @@
+% This script sets up the environment with VLFeat and LIBSVM
+% Note: you have to compile LIBSVM by going into its matlab folder (via
+% matlab) and running make.m. Then run this script from the final folder.
+
+if ~exist('svmtrain', 'file')
+    P = path;
+    path(P, 'libsvm-3.21/matlab');
+end
+
+if ~exist('vl_sift', 'file')
+    run('vlfeat-0.9.20/toolbox/vl_setup');
+end
